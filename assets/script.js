54f534158257ab46392afc24ac6d625c39d4046a
Houssine <ekhay1@outlook.com>
2024-04-03 11:26:59 -0400

    added javaScript file with varaible topics and array on it

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+const topic="HTML";
+const topics=['HTML','CSS','GIT','JavaScript'];
+
+for(let i=0;i<topics.length;i++){
+    console.log(topics[i]);
+}
+
+// if(topic === 'HTML'){
+//     console.log("Let's study HTML!");
+// }else if (topic==='CSS'){
+//     console.log("Let study CSS! ");
+// }else if (topic==='GIT'){
+//     console.log("Let study GIT! ");
+// }else if (topic==='JavaScript'){
+//     console.log("Let study JavaScript! ");
+// }else {
+//     console.log("Please try again! ");
+// }
